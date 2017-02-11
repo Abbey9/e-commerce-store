90f4f8291327b920b03f660224b70e629e903ce8
abhisheksingh7212@gmail.com <abhisheksingh7212@gmail.com>
2017-02-11 18:28:00 +0530

    Adding productRepository.getAllProducts just returns  a list of product domain objects and added this to model and returned the view products.jsp

--- eCommerceStore/src/main/java/com/myapps/ecommercestore/controller/ProductController.java
+++ eCommerceStore/src/main/java/com/myapps/ecommercestore/controller/ProductController.java
@@ -1,26 +1,22 @@
 package com.myapps.ecommercestore.controller;
 
-import java.math.BigDecimal;
-
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-import com.myapps.ecommercestore.domain.Product;
+
+import com.myapps.ecommercestore.domain.repository.ProductRepository;
 
 @Controller
 public class ProductController {
 	
+	@Autowired
+	private ProductRepository productRepository;
+	
 	@RequestMapping("/products")
 	public String list(Model model){
 		
-		Product book1=new Product("Lit001","The Adventures of Sherlock Holmes",new BigDecimal(500));
-		book1.setAuthor("Arthur Conan Doyle");
-		book1.setDescription("The Adventures of Sherlock Holmes is a collection of twelve short stories by Arthur Conan Doyle, featuring his fictional detective Sherlock Holmes. It was first published on 14 October 1892; the individual stories had been serialised in The Strand Magazine between June 1891 and July 1892.In general the stories in The Adventures of Sherlock Holmes identify, and try to correct, social injustices.");
-		book1.setCategory("Literature and Fiction");
-		book1.setPublisher("George Newnes");
-		book1.setUnitsInStock(100);
-		
-		model.addAttribute("product", book1);
+		model.addAttribute("products", productRepository.getAllProducts());
 		
 		return "products";
 	}
@@ -28,14 +24,7 @@ public class ProductController {
 	@RequestMapping("/products/viewProduct")
 	public String viewProducts(Model model){
 		
-		Product book1=new Product("Lit001","The Adventures of Sherlock Holmes",new BigDecimal(500));
-		book1.setAuthor("Arthur Conan Doyle");
-		book1.setDescription("The Adventures of Sherlock Holmes is a collection of twelve short stories by Arthur Conan Doyle, featuring his fictional detective Sherlock Holmes. It was first published on 14 October 1892; the individual stories had been serialised in The Strand Magazine between June 1891 and July 1892.In general the stories in The Adventures of Sherlock Holmes identify, and try to correct, social injustices.");
-		book1.setCategory("Literature and Fiction");
-		book1.setPublisher("George Newnes");
-		book1.setUnitsInStock(100);
-		
-		model.addAttribute("product", book1);
+		model.addAttribute("products", productRepository.getAllProducts());
 		
 		return "viewProduct";
 	}
