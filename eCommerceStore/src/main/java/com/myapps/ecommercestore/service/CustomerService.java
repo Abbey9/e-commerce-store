ec804157ba672e1e8a67219b7d0f5caab5fe06b6
abhisheksingh7212@gmail.com <abhisheksingh7212@gmail.com>
2017-02-12 19:22:37 +0530

    Adding another service layer named CustomerService to access persistence layer CustomerRepository

---
+++ eCommerceStore/src/main/java/com/myapps/ecommercestore/service/CustomerService.java
@@ -0,0 +1,11 @@
+package com.myapps.ecommercestore.service;
+
+import java.util.List;
+
+import com.myapps.ecommercestore.domain.Customer;
+
+public interface CustomerService {
+	
+	List<Customer> getAllCustomers();
+
+}
