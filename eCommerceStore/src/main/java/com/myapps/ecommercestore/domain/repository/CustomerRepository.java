f5c8543373a65fcca0a39ef7c3f8c288e552ec35
abhisheksingh7212@gmail.com <abhisheksingh7212@gmail.com>
2017-02-12 19:20:40 +0530

    Added a customer Dummy persistence Repository named CustomerRepository to store customer information

---
+++ eCommerceStore/src/main/java/com/myapps/ecommercestore/domain/repository/CustomerRepository.java
@@ -0,0 +1,11 @@
+package com.myapps.ecommercestore.domain.repository;
+
+import java.util.List;
+
+import com.myapps.ecommercestore.domain.Customer;
+
+public interface CustomerRepository {
+	
+	List<Customer> getAllCustomers();
+
+}
