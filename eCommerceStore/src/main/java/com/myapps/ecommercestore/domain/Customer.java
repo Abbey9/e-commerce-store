9e45e94fccd5e95efbb35d0c3453a4908237e9a7
abhisheksingh <abhisheksingh7212@gmail.com>
2017-05-06 17:54:34 +0530

    Added email and mobile fields in customer domain model

--- eCommerceStore/src/main/java/com/myapps/ecommercestore/domain/Customer.java
+++ eCommerceStore/src/main/java/com/myapps/ecommercestore/domain/Customer.java
@@ -6,45 +6,65 @@ public class Customer {
 	private String name;
 	private String address;
 	private long noOfOrdersMade;
-	
-	
+	private String email;
+	private String mobile;
+
 	public Customer() {
-		super();	
+		super();
 	}
-	
-	
+
 	public Customer(String customerId, String name) {
 		super();
 		this.customerId = customerId;
 		this.name = name;
 	}
 
-
 	public String getCustomerId() {
 		return customerId;
 	}
+
 	public void setCustomerId(String customerId) {
 		this.customerId = customerId;
 	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getMobile() {
+		return mobile;
+	}
+
+	public void setMobile(String mobile) {
+		this.mobile = mobile;
+	}
+
 	public String getName() {
 		return name;
 	}
+
 	public void setName(String name) {
 		this.name = name;
 	}
+
 	public String getAddress() {
 		return address;
 	}
+
 	public void setAddress(String address) {
 		this.address = address;
 	}
+
 	public long getNoOfOrdersMade() {
 		return noOfOrdersMade;
 	}
+
 	public void setNoOfOrdersMade(long noOfOrdersMade) {
 		this.noOfOrdersMade = noOfOrdersMade;
 	}
-	
-	
-	
+
 }
