d3c18f2fdb8ad74fa139b8f28ff84e1f403f96fa
abhisheksingh <abhisheksingh7212@gmail.com>
2017-05-06 17:48:10 +0530

    Added login and signUp requestMapping methods in home controller

--- eCommerceStore/src/main/java/com/myapps/ecommercestore/controller/HomeController.java
+++ eCommerceStore/src/main/java/com/myapps/ecommercestore/controller/HomeController.java
@@ -1,16 +1,36 @@
 package com.myapps.ecommercestore.controller;
 
 import org.springframework.stereotype.Controller;
-
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class HomeController {
 	
 	@RequestMapping("/")
-	public String welcome(){
-		
+	public String welcomePage(){
+	  
 	  return "welcome";
 	}
+	
+	@RequestMapping("/home")
+	public String welcomeLandingPage(){
+	  
+	  return "welcome";
+	}
+	
+	@RequestMapping("/login")
+	public String loginPage(){
+	  
+	  return "login";
+	}
+	
+	@RequestMapping("/signUp")
+	public String signUpPage(){
+	  
+	  return "signUp";
+	}
+
+
 
 }
