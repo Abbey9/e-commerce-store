41b54fdeba98ada809a3790735482543c24be28d
abhisheksingh <abhisheksingh7212@gmail.com>
2017-05-06 18:17:30 +0530

    Added one more request Mapping method to return the view based on category of product

--- eCommerceStore/src/main/java/com/myapps/ecommercestore/controller/ProductController.java
+++ eCommerceStore/src/main/java/com/myapps/ecommercestore/controller/ProductController.java
@@ -3,17 +3,22 @@ package com.myapps.ecommercestore.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
+import com.myapps.ecommercestore.domain.repository.ProductRepository;
 import com.myapps.ecommercestore.service.ProductService;
 
 @Controller
+@RequestMapping("/products")
 public class ProductController {
 	
 	@Autowired
 	private ProductService productService;
 	
-	@RequestMapping("/products")
+	
+	@RequestMapping
 	public String list(Model model){
 		
 		model.addAttribute("products", productService.getAllProducts());
@@ -21,5 +26,16 @@ public class ProductController {
 		return "products";
 	}
 	
-
+	@RequestMapping("/all")
+	public String allProducts(Model model){
+	    model.addAttribute("products", productService.getAllProducts());
+	    return "products";
+	}
+	
+	@RequestMapping("/{category}")
+	public String getProductByCategory(Model model, @PathVariable("category") String category){
+	    model.addAttribute("products", productService.getProductsByCategory(category));
+	    return "products";
+	}
+	
 }
