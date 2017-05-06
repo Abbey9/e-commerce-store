b01d2525bb52cfff19ef8a22767758e41a673d93
abhisheksingh <abhisheksingh7212@gmail.com>
2017-05-06 18:08:02 +0530

    Added few more customers into persistence layer

--- eCommerceStore/src/main/java/com/myapps/ecommercestore/domain/repository/impl/InMemoryCustomerRepository.java
+++ eCommerceStore/src/main/java/com/myapps/ecommercestore/domain/repository/impl/InMemoryCustomerRepository.java
@@ -15,18 +15,39 @@ public class InMemoryCustomerRepository implements CustomerRepository{
 	
     public InMemoryCustomerRepository(){
     	
-    	Customer customer1=new Customer("Cus001","Abhi Singh");
+    	Customer customer1=new Customer("Cus001","Abhishek Singh");
     	
-    	customer1.setAddress("Street11, New York, USA");
-    	customer1.setNoOfOrdersMade(2);
+    	customer1.setAddress("House-01, Street11, New York, USA");
+    	customer1.setNoOfOrdersMade(3);
+    	customer1.setEmail("abhisingh@gmail.com");
+    	customer1.setMobile("852779087");
     	
-        Customer customer2=new Customer("Cus002","Savita Singh");
+        Customer customer2=new Customer("Cus002","Manoj Kumar Singh");
     	
-    	customer2.setAddress("Street12, New York, USA");
-    	customer2.setNoOfOrdersMade(4);
+    	customer2.setAddress("House-02, Street11, New York, USA");
+    	customer2.setNoOfOrdersMade(6);
+    	customer2.setEmail("mksingh@gmail.com");
+    	customer2.setMobile("852798368");
+    	
+    	Customer customer3=new Customer("Cus003","Savita Singh");
+    	
+    	customer3.setAddress("House-02, Street11, New York, USA");
+    	customer3.setNoOfOrdersMade(9);
+    	customer3.setEmail("savitasingh@gmail.com");
+    	customer3.setMobile("852798377");
+    	
+       
+        Customer customer4=new Customer("Cus004","Abhijeet Singh");
+    	
+    	customer4.setAddress("House-03, Street11, New York, USA");
+    	customer4.setNoOfOrdersMade(7);
+    	customer4.setEmail("abhijeetsingh@gmail.com");
+    	customer4.setMobile("852798943");
     	
     	listOfCustomers.add(customer1);
     	listOfCustomers.add(customer2);
+    	listOfCustomers.add(customer3);
+    	listOfCustomers.add(customer4);
     	
     	
     }
